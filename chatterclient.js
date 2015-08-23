a390a0a2670f3788ed57680ecefa6cf3ebeea6b4
Måns Olsson <mans2111@gmail.com>
2015-08-23 10:14:38 +0200

    Added some styling and removed possible XSS

--- chatterclient.js
+++ chatterclient.js
@@ -3,7 +3,19 @@
 window.onload = function() {
     var ws = new WebSocket('ws://localhost:8080', 'chatter-protocol');
     ws.onmessage = function(event) {
-        document.getElementById('message-box').innerHTML += '<p>' + event.data + '</p>';
+        var messageMetaDiv = document.createElement('div');
+        messageMetaDiv.className = 'message-meta';
+        messageMetaDiv.appendChild(document.createTextNode('Test Testsson'));
+        messageMetaDiv.appendChild(document.createElement('br'));
+        messageMetaDiv.appendChild(document.createTextNode('2014-01-01 11:12'));
+
+        var messageDiv = document.createElement('div');
+        messageDiv.className = 'message';
+        messageDiv.appendChild(document.createTextNode(event.data));
+        messageDiv.appendChild(document.createElement('br'));
+        messageDiv.appendChild(messageMetaDiv);
+
+        document.getElementById('message-box').appendChild(messageDiv);
     };
     ws.onopen = function() {
         document.getElementById('ib').onclick = function(event) {
